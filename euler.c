c0f19b56c45c598f0b513e98caca8b749a453ba9
Sarah Leong <sleong20@stuy.edu>
2021-09-25 13:48:22 -0400

    added euler.c with function definitions

---
+++ euler.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int problem1() {
+    int sum = 0;
+    int x;
+    for (x = 0; x < 1000; x++) {
+        if ((x % 3 == 0) || (x % 5 == 0)) {
+            sum += x;
+        }
+    }
+    return sum;
+}
+
+int problem5() {
+    int ans;
+    int found = 1;
+    int x;
+    for (ans=2520; found != 0; ans++) {
+        found = 0;
+        for (x = 1; x < 21; x++) {
+            if ((ans % x) != 0) {
+                found = 1;
+                x = 21;
+            }
+        }
+    }
+    // it adds an extra 1 before terminating?
+    return ans-1;
+}
