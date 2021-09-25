a5191c40207025ba259e00551ea336c23f56bcfb
Sarah Leong <sleong20@stuy.edu>
2021-09-25 14:54:58 -0400

    changed function call to proper name

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include "euler.h"
 
 int main() {
-    printf("Answer to Program 1: %d \n", program1());
-    printf("Answer to Program 5: %d \n", program5());
+    printf("Answer to Program 1: %d \n", problem1());
+    printf("Answer to Program 5: %d \n", problem5());
+    return 0;
 }
